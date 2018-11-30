bc3e209e46140ba1ac1472c501e2aa9c13dc551d
gadirllh <gadirlih@gmail.com>
2018-12-01 00:35:37 +0400

    Create Game - Initial Commit
    Created game: added circuit, test car.
    Wrote CameraController specific for this car game.

---
+++ GameOps-Project/Assets/Scripts/CameraController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+	
+    public Transform car;
+    public Vector3 cameraOffset;
+    public float cameraFollowSpeed = 10;
+    public float cameraLookSpeed = 10;
+
+    void Start()
+    {
+        cameraOffset = transform.position - car.position;
+    }
+
+    public void LookAtTarget()
+    {
+        Vector3 _lookDirection = car.position - transform.position;
+        Quaternion _rot = Quaternion.LookRotation(_lookDirection, Vector3.up);
+        transform.rotation = Quaternion.Lerp(transform.rotation, _rot, cameraLookSpeed * Time.deltaTime);
+    }
+
+    public void MoveToTarget()
+    {
+        Vector3 _targetPos = car.position +
+                             car.forward * cameraOffset.z +
+                             car.right * cameraOffset.x +
+                             car.up * cameraOffset.y;
+        transform.position = Vector3.Lerp(transform.position, _targetPos, cameraFollowSpeed * Time.deltaTime);
+    }
+
+    private void FixedUpdate()
+    {
+        LookAtTarget();
+        MoveToTarget();
+    }
+
+}
