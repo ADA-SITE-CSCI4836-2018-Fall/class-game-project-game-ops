c4311f78eec44c0f0b950feb35569c79021718de
gadirllh <gadirlih@gmail.com>
2018-12-01 03:09:57 +0400

    Complete lap time counter
    created LapTimeController to control text in the lap time panel and update them. LapTimeRecord to update record time

---
+++ GameOps-Project/Assets/Scripts/LapTimeController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LapTimeController : MonoBehaviour {
+    public static int minutesPassed;
+    public static int secondsPassed;
+    public static float millisecondsPassed;
+    public static string millisecondsInStrind;
+
+    public GameObject minuteText;
+    public GameObject secondText;
+    public GameObject milliseondsText;
+
+
+    void Update()
+    {
+        millisecondsPassed += Time.deltaTime * 10;
+        millisecondsInStrind = millisecondsPassed.ToString("F0");
+        milliseondsText.GetComponent<Text>().text = "" + millisecondsInStrind;
+
+        if (millisecondsPassed >= 10)
+        {
+            millisecondsPassed = 0;
+            secondsPassed += 1;
+        }
+
+        if (secondsPassed <= 9)
+        {
+            secondText.GetComponent<Text>().text = "0" + secondsPassed + ".";
+        }
+        else
+        {
+            secondText.GetComponent<Text>().text = "" + secondsPassed + ".";
+        }
+
+        if (secondsPassed >= 60)
+        {
+            secondsPassed = 0;
+            minutesPassed += 1;
+        }
+
+        if (minutesPassed <= 9)
+        {
+            minuteText.GetComponent<Text>().text = "0" + minutesPassed + ":";
+        }
+        else
+        {
+            minuteText.GetComponent<Text>().text = "" + minutesPassed + ":";
+        }
+
+    }
+}
---
+++ GameOps-Project/Assets/Scripts/LapTimeRecord.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LapTimeRecord : MonoBehaviour {
+    public GameObject halfWayTrigger;
+    public GameObject lapCompleteTrigger;
+
+    public GameObject MinuteDisplay;
+    public GameObject SecondDisplay;
+    public GameObject MilliDisplay;
+
+    public GameObject LapTimeBox;
+
+    void OnTriggerEnter()
+    {
+
+        if (LapTimeController.secondsPassed <= 9)
+        {
+            SecondDisplay.GetComponent<Text>().text = "0" + LapTimeController.secondsPassed + ".";
+        }
+        else
+        {
+            SecondDisplay.GetComponent<Text>().text = "" + LapTimeController.secondsPassed + ".";
+        }
+
+        if (LapTimeController.minutesPassed <= 9)
+        {
+            MinuteDisplay.GetComponent<Text>().text = "0" + LapTimeController.minutesPassed + ".";
+        }
+        else
+        {
+            MinuteDisplay.GetComponent<Text>().text = "" + LapTimeController.minutesPassed + ".";
+        }
+
+        MilliDisplay.GetComponent<Text>().text = "" + LapTimeController.millisecondsPassed.ToString("F0");
+
+        LapTimeController.minutesPassed = 0;
+        LapTimeController.secondsPassed = 0;
+        LapTimeController.millisecondsPassed = 0;
+
+        halfWayTrigger.SetActive(true);
+        lapCompleteTrigger.SetActive(false);
+    }
+}
