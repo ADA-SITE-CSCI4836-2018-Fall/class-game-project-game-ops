7baea2fe658083faa4fef9f0e880c8d7f597e587
gadirllh <gadirlih@gmail.com>
2018-12-01 02:21:48 +0400

    Create Finish Line
    Created finish line and added trigger to help to calculate lap time and lrecord time

---
+++ GameOps-Project/Assets/Scripts/HalfWayTrigger.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HalfWayTrigger : MonoBehaviour {
+
+    public GameObject halfWayTrigger;
+    public GameObject lapCompleteTrigger;
+
+	void OnTriggerEnter()
+    {
+        halfWayTrigger.SetActive(false);
+        lapCompleteTrigger.SetActive(true);
+    }
+}
