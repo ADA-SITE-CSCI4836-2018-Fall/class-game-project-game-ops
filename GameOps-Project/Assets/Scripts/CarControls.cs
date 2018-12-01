8d31d68256a2457238c4c7252bf1fd8d4fc44428
Musadiq Aliyev <musadiq.aliyev@gmail.com>
2018-12-01 17:19:57 +0400

    AI fixing issue
    Now,AI car starting in same time as driver

--- GameOps-Project/Assets/Scripts/CarControls.cs
+++ GameOps-Project/Assets/Scripts/CarControls.cs
@@ -7,10 +7,12 @@ namespace UnityStandardAssets.Vehicles.Car
     public class CarControls : MonoBehaviour
     {
         public CarController car; // reference to the car controller, must be dragged in inspector
+        public CarAIControl AIDriver01;
         
         private void Start()
         {
             car.enabled = true;
+            AIDriver01.enabled = true;
         }
     }
 }
